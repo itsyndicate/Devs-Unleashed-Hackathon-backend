0591b55837f7a234b81c5bbf94a218f28b655bef
Alex Pitenko <oleksii.p@itsyndicate.org>
2023-01-04 00:05:09 +0100

    implemented necessary models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE, related_name='profile')
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Taskogotchi(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='taskogotchi')
+    project = models.ForeignKey('Project', on_delete=models.CASCADE)
+    image = models.JSONField('Image components stored as JSON', null=True, blank=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    health = models.IntegerField(default=100)
+    strength = models.IntegerField(default=100)
+
+    class Meta:
+        unique_together = ('user', 'project')
+
