fc7d6d5d05f81dafc6198b1d30668967536adb0e
Alex Pitenko <oleksii.p@itsyndicate.org>
2023-01-05 22:54:49 +0100

    finished models

--- core/models.py
+++ core/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 
+class Project(models.Model):
+    account_id = models.CharField('accountId from Jira', max_length=43)
+    name = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class UserProfile(models.Model):
-    user = models.OneToOneField('auth.User', on_delete=models.CASCADE, related_name='profile')
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='profiles')
+    project = models.ForeignKey('Project', on_delete=models.CASCADE, related_name='profiles')
 
     def __str__(self):
         return self.user.username
@@ -12,16 +21,35 @@ class UserProfile(models.Model):
         verbose_name_plural = 'Profiles'
 
 
-class Project(models.Model):
-    name = models.CharField(max_length=200)
+class FightStatus(models.TextChoices):
+    WAITING_ACCEPT = 'WA', 'Waiting for accept'
+    ACCEPTED = 'A', 'Accepted'
+    PENDING = 'P', 'Pending'
+    COMPLETED = 'CO', 'Completed'
+    CANCELED = 'CA', 'Canceled'
+
+
+class FightChallenge(models.Model):
+    initiator = models.ForeignKey('UserProfile', on_delete=models.CASCADE, related_name='initiated_fights')
+    opponent = models.ForeignKey('UserProfile', on_delete=models.CASCADE, related_name='received_fights',
+                                 null=True, blank=True)
+    status = models.CharField(max_length=2, choices=FightStatus.choices, default=FightStatus.WAITING_ACCEPT)
+    winner = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='won_fights', null=True,
+                               blank=True)
+    draw = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        assert self.initiator != self.opponent, "You can't fight with yourself"
+        assert self.initiator.project == self.opponent.project, "You can't fight with someone from another project"
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.name
+        return f"Fight between {self.initiator} and {self.opponent}"
 
 
 class Taskogotchi(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='taskogotchi')
-    project = models.ForeignKey('Project', on_delete=models.CASCADE)
+    project = models.OneToOneField('Project', on_delete=models.CASCADE)
     image = models.JSONField('Image components stored as JSON', null=True, blank=True)
     last_updated = models.DateTimeField(auto_now=True)
     health = models.IntegerField(default=100)
@@ -29,4 +57,3 @@ class Taskogotchi(models.Model):
 
     class Meta:
         unique_together = ('user', 'project')
-
