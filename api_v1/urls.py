991f30e0973f9f026108b7acd19f9034604d6bc7
Alex Pitenko <oleksii.p@itsyndicate.org>
2023-01-10 20:12:49 +0100

    changed path to re_path in urls

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import re_path
 from .views import RegisterPlayerView, TaskogotchiView, OpponentsListView, FightChallengeView
 
 urlpatterns = [
-    path('register-player', RegisterPlayerView.as_view(), name='register-player'),
-    path('taskogotchi', TaskogotchiView.as_view(), name='taskogotchi'),
-    path('available-opponents', OpponentsListView.as_view(), name='available-opponents'),
-    path('fight', FightChallengeView.as_view(), name='fight'),
+    re_path(r'register-player/?', RegisterPlayerView.as_view(), name='register-player'),
+    re_path(r'taskogotchi/?', TaskogotchiView.as_view(), name='taskogotchi'),
+    re_path(r'available-opponents/?', OpponentsListView.as_view(), name='available-opponents'),
+    re_path(r'fight/?', FightChallengeView.as_view(), name='fight'),
 ]
